e5fff36a15f075a85b0fb1a67bdc78e2ac355fdf
Hugo Bezerra <hugoricardo.so.be@gmail.com>
2019-11-22 10:36:15 -0300

    organizing stuff

---
+++ languages/python/renaming-my-uri-files.py
@@ -0,0 +1,35 @@
+import os 
+      
+for filename in os.listdir("backup-uri"): 
+    if(filename[0] == 'u'):
+        src = 'backup-uri/' + filename 
+        dst = filename[3:len(filename)]
+        os.rename(src, dst) 
+
+'''
+# Pythono3 code to rename multiple  
+# files in a directory or folder 
+  
+# importing os module 
+import os 
+  
+# Function to rename multiple files 
+def main(): 
+    i = 0
+      
+    for filename in os.listdir("xyz"): 
+        dst ="Hostel" + str(i) + ".jpg"
+        src ='xyz'+ filename 
+        dst ='xyz'+ dst 
+          
+        # rename() function will 
+        # rename all the files 
+        os.rename(src, dst) 
+        i += 1
+  
+# Driver Code 
+if __name__ == '__main__': 
+      
+    # Calling main() function 
+    main() 
+'''
